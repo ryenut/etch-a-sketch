d6f34daf9ce339354d0a014e00d653ecb0fb89ab
ryenut <erikamaegalo@gmail.com>
2022-11-01 17:40:45 +0800

    Create pen and color function

--- etch2.js
+++ etch2.js
@@ -3,7 +3,6 @@ const inputGrid = document.querySelector('.user-input');
 const select = document.querySelector('select');
 const clear = document.querySelector('.reset');
 
-
 function generateGrid(px) {
 
     let createGrid = px * px
@@ -11,14 +10,34 @@ function generateGrid(px) {
     grid.style.gridTemplateColumns = `repeat(${px}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${px}, 1fr)`;
 
+
     for (let i = 1; i <= createGrid; i++) {
         let div = document.createElement('div');
         div.classList.add('square')
         grid.insertAdjacentElement('beforeend', div);
-    }
-};
+        
+        function penColor(bgColor) {
+            div.addEventListener('mouseover', () => {                               
+                    div.style.backgroundColor = bgColor;
+        });
 
-generateGrid(16);
+    }
+
+        select.addEventListener('change', () => {
+            const choice = select.value;
+            const random = '#' + Math.floor(Math.random()*16777215).toString(16);
+        
+            switch (choice) {
+                case 'black':
+                    penColor('black');
+                    break;
+                case 'random':
+                    penColor(random);
+            } 
+        })
+    }
+}
+
 
 function getInput() {
 
@@ -33,7 +52,11 @@ function getInput() {
     }
 }
 
+generateGrid(16);
+
 document.addEventListener('DOMContentLoaded', () => {
+
+
     inputGrid.addEventListener('click', () => {
         let px = getInput()
         generateGrid(px);
